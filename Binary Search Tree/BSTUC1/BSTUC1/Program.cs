f732a9872994be5b3f1c2a8ed5138e3b2d2d2b65
sagarjaid23 <sagarjaid23@gmail.com>
2022-03-15 01:20:44 +0530

    Assignment 15

---
+++ Binary Search Tree/BSTUC1/BSTUC1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static BSTUC1.MapNode;
+
+namespace BSTUC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BTS Operation");
+            Console.WriteLine("1.Insertion.");
+            
+            int num = Convert.ToInt32(Console.ReadLine());
+            BST<int> binarySearchTree = new BST<int>(56);
+            switch (num)
+            {
+                case 1:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Binary Search Tree/BSTUC2/BSTUC2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static BSTUC2.MyNode;
+
+namespace BSTUC2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BTS Operation");
+            Console.WriteLine("1.Insertion.");
+            Console.WriteLine("2.Insert More Elements");
+
+            Console.WriteLine("Enter the option!!!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            BST<int> binarySearchTree = new BST<int>(56);
+            switch (num)
+            {
+                case 1:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Display();
+                    break;
+                case 2:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Insert(22);
+                    binarySearchTree.Insert(40);
+                    binarySearchTree.Insert(11);
+                    binarySearchTree.Insert(3);
+                    binarySearchTree.Insert(16);
+                    binarySearchTree.Insert(60);
+                    binarySearchTree.Insert(95);
+                    binarySearchTree.Insert(65);
+                    binarySearchTree.Insert(63);
+                    binarySearchTree.Insert(67);
+                    binarySearchTree.Display();
+                    binarySearchTree.GetSize();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
